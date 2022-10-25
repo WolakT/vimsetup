1f4890de3a091d7dec16334480edd982d3d57afb
tomasz.wolak <tomasz.wolak@Admins-MacBook-Pro.local>
2022-10-25 10:41:18 +0200

    changed the template so that it works

--- template_dag.py
+++ template_dag.py
@@ -1,7 +1,9 @@
 from airflow.decorators import dag, task
+from datetime import datetime
+
 @dag(
     schedule=None,
-    start_date=pendulum.datetime(2022, 9,21, tz="UTC"),
+    start_date=datetime(2022, 10, 21, hour=8),
     catchup=False,
     tags=['demo'],
 )
@@ -11,8 +13,12 @@ def template_dag():
     @task()
     def example_func():
         pass
-   @task(multiple_outputs=True)
+    @task(multiple_outputs=True)
     def transform(order_data_dict: dict):
-        pass
-
+        return order_data_dict
+    
+    t1 = example_func()
+    t2 = transform({'3': 3})
+    t1 >> t2
 template_dag()
+
